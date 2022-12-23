b93f9c2c2972ced3208d0fb01287884cf46cb8b9
Zavier Ferodova Al Fitroh <zavierferodova@gmail.com>
2022-12-23 18:31:34 +0700

    Fix client infinite print loop after connection disconnected

--- client.py
+++ client.py
@@ -4,10 +4,20 @@ from threading import Thread
 HOST = 'localhost'
 PORT = 50001
 
-def listen_for_messages(socket):
+def listen_for_messages(s: socket):
+    empty_response = 0
     while True:
-        msg = socket.recv(1024).decode() # receive server response
-        print(msg)
+        response = s.recv(1024) # receive server response
+        msg = response.decode()
+
+        if msg != "":
+            print(msg) # print messages
+        else:
+            empty_response += 1
+
+        if empty_response > 10:
+            print("Connection disconnected, press enter to continue!")
+            exit(0)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(f'Connecting with {HOST}:{PORT} ...')
@@ -21,10 +31,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     t.start()
 
     while True:
-        msg_input = input()
-        if (msg_input.lower() == "/cmd exit"):
-            s.close()
+        try:
+            msg_input = input()
+            if (msg_input.lower() == "/cmd exit"):
+                s.close()
+                exit(0)
+            else:
+                msg = f"{username}> {msg_input}"
+                s.send(bytes(msg.encode())) # send message
+        except ValueError: # I/O exception because program is exit after empty responses
             exit(0)
-        else:
-            msg = f"{username}> {msg_input}"
-            s.send(bytes(msg.encode())) # send command
--- server.py
+++ server.py
@@ -7,19 +7,20 @@ PORT = 50001
 client_connections = set()
 max_conn = 100
 
-def remove_connection(conn):
+def remove_connection(conn: socket):
     client_connections.remove(conn)
 
-def broadcast_message(msg):
+def broadcast_message(msg: str):
     for client_connection in client_connections:
         client_connection.send(msg.encode())
 
-def listen_for_client(conn):
+def listen_for_client(conn: socket):
     while True:
         try:
             bytes_data = conn.recv(1024) # receive data
             msg = bytes_data.decode()
-            broadcast_message(msg)
+            if msg != "":
+                broadcast_message(msg)
         except Exception as e:
             remove_connection(conn)
             break
@@ -31,9 +32,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print('Waiting clients connection ...')
 
     while True: 
-        conn, addr = s.accept() # accept client connection
-        print(f'Connected by {addr}')
-        client_connections.add(conn)
-        t = Thread(target=listen_for_client, args=(conn,))
-        t.daemon = True
-        t.start()
+        try:
+            conn, addr = s.accept() # accept client connection
+            print(f'Connected by {addr}')
+            client_connections.add(conn)
+            t = Thread(target=listen_for_client, args=(conn,))
+            t.daemon = True
+            t.start()
+        except KeyboardInterrupt:
+            print("\nDisconnected by user!")
+            s.close()
+            exit()
